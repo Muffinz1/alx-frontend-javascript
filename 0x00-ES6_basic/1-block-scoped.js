28b9d91b17ad4e4b3310f19a935c41ef95335410
Muffinz1 <amiryou120@gmail.com>
2024-06-21 04:05:57 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,14 @@
-/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
 	const task = false;
 	const task2 = true;
-
+  
 	if (trueOrFalse) {
-	const task = true;
-	const task2 = false;
-		    }
-
-	  return [task, task2];
-}
+	  const task = true;
+	  const task2 = false;
+	  if (task && task2) {
+		return [task, task2];
+	  }
+	}
+  
+	return [task, task2];
+  }

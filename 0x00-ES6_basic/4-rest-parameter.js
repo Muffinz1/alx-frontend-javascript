352b5a4605a58076d99c76fa6b2bdd528e40cd51
Muffinz1 <amiryou120@gmail.com>
2024-06-21 04:23:33 +0300

    check string interpolation

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,3 @@
-import getSumOfHoods from './3-default-parameter.js';
-
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
+export default function returnHowManyArguments(...args) {
+    return args.length;
+  }
